7f6833955e2a01604b180a9b83328e0d96afecdc
Sherif <sherifatitebi@gmail.com>
2025-04-09 23:58:46 +0100

    can stream and read. lock read loop while streaming

--- p2p/message.go
+++ p2p/message.go
@@ -1,8 +1,14 @@
 package p2p
 
-//Message holds any arbitrary data that is being sent over
+const (
+	IncomingMessage = 0x1
+	IncomingStream  = 0x2
+)
+
+// Message holds any arbitrary data that is being sent over
 // each transport netween two nodes in the network
 type RPC struct {
 	From    string
 	Payload []byte
+	Stream  bool
 }
